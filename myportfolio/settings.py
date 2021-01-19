98cc7aa98f920d2d2fafdff2bda7d4d9de3164ba
drnorfaizal <drnorfaizal@gmail.com>
2021-01-19 10:39:16 +0000

    remove stuff

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@e+$^9o(3&xw6f*k4gg_=xg10r#9b*pg_mo&6d6b5hck_9ty@p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['drnorfaizalab.pythonanywhere.com']
 
 # Application definition
 
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
